53a123300379bbda57ad72651b0cf901d4146053
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-10-06 11:16:10 +0300

    add custom button

---
+++ src/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,9 @@
+import CustomButtonStyled from "./CustomButtonStyled"
+
+const CustomButton = () => {
+    return(
+        <CustomButtonStyled variant="contained">Hello World</CustomButtonStyled>
+    );
+};
+
+export default CustomButton;
---
+++ src/components/CustomButton/CustomButtonStyled.tsx
@@ -0,0 +1,26 @@
+import Button, { ButtonProps } from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+const CustomButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: "pink",
+  },
+}));
+
+export default CustomButtonStyled;
+
+
+// import Button, { ButtonProps } from "@mui/material/Button";
+// import { styled } from "@mui/material/styles";
+
+// const CustomButtonStyled = styled(Button)<ButtonProps>(({theme}) => ({
+//     backgroundColor: theme.palette.primary.main,
+//     color: theme.palette.primary.contrastText,
+//     '&:hover': {
+//         backgroundColor: "pink"
+//     },
+// }))
+
+// export default CustomButtonStyled
